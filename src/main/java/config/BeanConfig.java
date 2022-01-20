c0070a4fb6b1193c37803b30ac3da33fa387ddb3
hongyong <hyhongyong@foxmail.com>
2022-01-20 23:40:24 +0800

    移除ComponentScan，基于JavaConfig的显示配置

--- src/main/java/config/BeanConfig.java
+++ src/main/java/config/BeanConfig.java
@@ -1,11 +1,22 @@
 package config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pojo.cd.CompactDisk;
+import pojo.cd.Fantasy;
+import pojo.player.CDPlayer;
 import pojo.player.MediaPlayer;
 
+//@ComponentScan(basePackageClasses = {CompactDisk.class, MediaPlayer.class})
 @Configuration
-@ComponentScan(basePackageClasses = {CompactDisk.class, MediaPlayer.class})
 public class BeanConfig {
+    @Bean  // 基于JavaConfig的显示配置
+    public CompactDisk fantasy() {
+        return new Fantasy();
+    }
+
+    @Bean
+    public MediaPlayer cdPlayer() {
+        return new CDPlayer();
+    }
 }
