33999d43fd6f0686b1c562b74c3aec8a6e9aa369
hongyong <hyhongyong@foxmail.com>
2022-01-21 09:51:24 +0800

    @Bean注解添加在方法上，可以拦截构造方法的调用，并且默认是单例模式

--- src/main/java/config/BeanConfig.java
+++ src/main/java/config/BeanConfig.java
@@ -1,29 +1,32 @@
 package config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import pojo.cd.CompactDisk;
 import pojo.cd.Fantasy;
 import pojo.player.CDPlayer;
 import pojo.player.MediaPlayer;
 
-//@ComponentScan(basePackageClasses = {CompactDisk.class, MediaPlayer.class})
+@ComponentScan(basePackageClasses = {CompactDisk.class, MediaPlayer.class})
 @Configuration
 public class BeanConfig {
     @Bean  // 基于JavaConfig的显示配置
     public CompactDisk fantasy() {
-        Integer choice = Integer.valueOf((int) Math.floor(Math.random() * 3));
+        Integer choice = Integer.valueOf((int) Math.floor(Math.random() * 4));
         switch (choice) {  // 发挥Java的强大威力
             case 0:
                 return new Fantasy();
             case 1:
                 return new Fantasy("珊瑚海", 150.00);
+            case 2:
+                return new Fantasy("斗牛要不要", 200.00);
             default:
                 return new Fantasy("七里香", 120.00);
         }
     }
 
-    @Bean(name = "cDPlayer")
+    @Bean(name = "anotherCDPlayer")
     public MediaPlayer cdPlayer() {
         return new CDPlayer();
     }
--- src/test/java/pojo/player/CDPlayerTest.java
+++ src/test/java/pojo/player/CDPlayerTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,10 +13,24 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(classes = BeanConfig.class)
 public class CDPlayerTest extends TestCase {
     @Autowired
+    @Qualifier("anotherCDPlayer")
+    private MediaPlayer anotherMediaPlayer;
+
+    @Autowired
+    @Qualifier("CDPlayer")
     private MediaPlayer mediaPlayer;
 
     @Test
     public void testPlay() {
         mediaPlayer.play();
+        anotherMediaPlayer.play();
+    }
+
+    @Test
+    public void testEquals() {
+        // 单例模式
+        System.out.println(anotherMediaPlayer.hashCode());
+        System.out.println(mediaPlayer.hashCode());
+        System.out.println(anotherMediaPlayer.equals(mediaPlayer));
     }
 }
